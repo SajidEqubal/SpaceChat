983ad2147d89c32e609f0210d9b6bfd22efd4f18
Shajid Equbal <sajidshad8@gmail.com>
2023-11-11 21:59:30 +0530

    First init

---
+++ app/src/main/java/com/spaceneochat/admin/Login.java
@@ -0,0 +1,16 @@
+package com.spaceneochat.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.spaceneochat.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/spaceneochat/admin/Splash.java
@@ -0,0 +1,32 @@
+package com.spaceneochat.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.spaceneochat.R;
+
+public class Splash extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIME_OUT=4000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN_TIME_OUT);
+    }
+}
