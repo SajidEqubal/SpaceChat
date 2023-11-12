32db4280513050adc327306f175b7815a696b6c4
Shajid Equbal <sajidshad8@gmail.com>
2023-11-12 19:00:51 +0530

    Firebase SetUp Done

--- app/src/main/java/com/spaceneochat/admin/SignIn.java
+++ app/src/main/java/com/spaceneochat/admin/SignIn.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.spaceneochat.R;
 import com.spaceneochat.databinding.ActivitySignInBinding;
 
+import java.util.HashMap;
+
 public class SignIn extends AppCompatActivity {
+
+    MaterialButton Button;
     private ActivitySignInBinding binding;
 
     @Override
@@ -18,10 +26,40 @@ public class SignIn extends AppCompatActivity {
         setContentView(binding.getRoot());
         setContentView(R.layout.activity_sign_in);
         setListeners();
+
+        Button = findViewById(R.id.buttonSignIn);
+        Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SignIn.this, "Text", Toast.LENGTH_SHORT).show();
+                addDataToFirebase();
+            }
+        });
     }
 
+
+    //Binding is not working fix this error
     private void setListeners() {
         binding.textCreateNewAccount.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(),SignUp.class)));
+        binding.buttonSignIn.setOnClickListener(v ->addDataToFirebase());
+
     }
+
+    private void addDataToFirebase(){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("first_name", "Shad");
+        data.put("last_name", "Space");
+        database.collection("users")
+                .add(data)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(getApplicationContext(),"Data Entered", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(exception ->{
+                    Toast.makeText(getApplicationContext(), exception.getMessage(),Toast.LENGTH_SHORT).show();
+                });
+
+    }
+
 }
